239fb6c5e30cbc89f53f6bc23d382d338d343e4b
hgkdd <75255150+hgkdd@users.noreply.github.com>
2025-01-21 12:08:25 +0100

    continious rotation

--- StirrerRC.py
+++ StirrerRC.py
@@ -1,10 +1,12 @@
 import sys
 
+from PySide6 import QtCore
 from PySide6.QtCore import (Qt, QLocale, QSettings)
 from PySide6.QtGui import (QStandardItemModel, QStandardItem)
 from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox,
                                QFileDialog, QTableWidgetItem, QVBoxLayout, QWidget)
 
+from stirrer import Stirrer
 
 # Important:
 # You need to run the following command to generate the mainwindow.py file
@@ -21,6 +23,45 @@ class MainWindow(QMainWindow):
         # register message box for quit action
         self.ui.actionQuit.triggered.connect(MainWindow.close)
 
+        self.ui.init_pushButton.clicked.connect(self.init_clicked)
+        self.ui.velocity_spinBox.valueChanged.connect(self.velocity_changed)
+        self.ui.stop_pushButton.clicked.connect(self.stopp_clicked)
+        self.ui.stirmode_start_pushButton.clicked.connect(self.stirrmode_start_clicked)
+
+        self.stirrer = Stirrer()
+        self.velocity = 50 # in percent
+        self.is_initialized = False
+
+        curpos_timer = QtCore.QTimer()
+        curpos_timer.setInterval(500)
+        curpos_timer.timeout.connect(self._update_position)
+        curpos_timer.start()
+
+    def init_clicked(self):
+        self.stirrer.initialize_drive()
+        self.is_initialized = self.stirrer.drive_initialized
+        self._update_position()
+
+    def velocity_changed(self):
+        self.velocity = self.ui.velocity_spinBox.value()
+
+    def stopp_clicked(self):
+        self.stirrer.stop_motor()
+        self._update_position()
+
+    def _update_position(self):
+        if self.is_initialized:
+            pos = self.stirrer.current_angle
+            self.ui.cur_pos_label.setText(str(pos))
+
+    def stirrmode_start_clicked(self):
+        if self.is_initialized:
+            vel = self.ui.velocity_spinBox.value()   # currently ignored
+            if self.ui.stirrmode_cw_radioButton.isChecked():
+                self.stirrer.run_clockwise()
+            else:
+                self.stirrer.run_anti_clockwise()
+
     def closeEvent(self, event):
         # fire confirmation box
         ret = QMessageBox.question(self, "StirrerRC",
@@ -28,6 +69,7 @@ class MainWindow(QMainWindow):
                                            QMessageBox.StandardButton.Yes, QMessageBox.StandardButton.No)
         if ret == QMessageBox.StandardButton.Yes:
             # self._save_setup()
+            self.stirrer.stop_motor()
             event.accept()
         else:
             event.ignore()
