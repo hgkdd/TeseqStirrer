4b030ee6449f7e714363366be3fa526a362fef33
hgkdd <75255150+hgkdd@users.noreply.github.com>
2025-02-23 20:44:35 +0100

    unused import statements removed

--- StirrerRC.py
+++ StirrerRC.py
@@ -1,10 +1,8 @@
 import sys
 
 from PySide6 import QtCore
-from PySide6.QtCore import (Qt, QLocale, QSettings)
-from PySide6.QtGui import (QStandardItemModel, QStandardItem)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox,
-                               QFileDialog, QTableWidgetItem, QVBoxLayout, QWidget)
+from PySide6.QtCore import (QLocale, QSettings)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox)
 
 from stirrer import Stirrer
 
